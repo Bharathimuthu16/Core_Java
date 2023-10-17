5d9b87bf268047e7b132be96aeeb2b632760b01b
91894 <91894@LAPTOP-UL4G2920>
2023-10-17 22:01:32 +0530

    First Commit

---
+++ src/com/student/Student.java
@@ -0,0 +1,10 @@
+package com.student;
+
+public class Student {
+
+	public static void main(String[] args) {
+		String name="Bharathi Muthu";
+		System.out.println("The Student name is:"+name);
+	}
+
+}
